409cf4edf01b8615b1b88e72327eb838cf48f65b
sa <psa5013@hanmail.com>
2020-05-20 16:12:16 +0900

    router-link

--- src/components/Route.vue
+++ src/components/Route.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         aaaaa
-        <!-- <router-link to="/route1">A보임</router-link>
-        <router-link to="/route2">B보임</router-link> -->
+        <router-link to="/route1">A보임</router-link>
+        <router-link to="/route2">B보임</router-link>
     </div>
 </template>
 
